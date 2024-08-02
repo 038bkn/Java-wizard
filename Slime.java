959ca1e4f30c7ffe61f8dc35e5f042a2e4cfdf5b
038bkn <038bkn@gmail.com>
2024-08-02 22:04:07 +0900

    feat: Add attack method to Slime class

--- Slime.java
+++ Slime.java
@@ -1,3 +1,12 @@
 public class Slime extends Object {
     
+    int hp;
+
+    public int attack(int wizardHp) {
+        System.out.println("スライムの攻撃！");
+        System.out.println("魔法使いに5ポイントのダメージを与えた！");
+        wizardHp -= 5;
+        return wizardHp;
+    }
+
 }
