8586730bef67e2b3a2c5a4e1c30441336a417729
038bkn <038bkn@gmail.com>
2024-08-03 18:44:18 +0900

    いろいろ変更、追加、ファイル分け

--- WizardFire.java
+++ WizardFire.java
@@ -2,9 +2,9 @@ public class WizardFire extends Wizard {
 
     @Override
     public int attack(int targetHp) {
-        System.out.println("ファイヤーボール！！！");
-        System.out.println("スライムに5ポイントのダメージを与えた！");
-        targetHp -= 5;
+        System.out.println("漆黒の闇の炎【ダークフレイム・マスターストライク】で攻撃した！");
+        System.out.println("スライムに3ポイントのダメージを与えた！");
+        targetHp -= 3;
         return targetHp;
     }
 }
--- WizardWater.java
+++ WizardWater.java
@@ -2,8 +2,8 @@ public class WizardWater extends Wizard {
 
     @Override
     public int attack(int targetHp) {
-        System.out.println("水鉄砲ぶしゃー！！！");
-        System.out.println("スライムに5ポイントのダメージを与えた！");
+        System.out.println("氷結の波動【フロストウェーブ】！！！");
+        System.out.println("スライムに3ポイントのダメージを与えた！");
         targetHp -= 5;
         return targetHp;
     }
