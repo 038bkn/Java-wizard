b02c87754171a9bb51110a048504855b672e8d37
038bkn <038bkn@gmail.com>
2024-08-05 13:05:14 +0900

    コメント追加

--- Enemy.java
+++ Enemy.java
@@ -55,7 +55,7 @@ public abstract class Enemy extends Object {
      * @return targetHp
      */
     public int attack(Random random, int targetHp) {
-        int attackType = Main.random.nextInt(3); // 0～2の乱数を生成
+        int attackType = random.nextInt(3); // 0～2の乱数を生成
         switch (attackType) {
             case 0:
                 return attack1(targetHp);
@@ -64,16 +64,36 @@ public abstract class Enemy extends Object {
             case 2:
                 return attack3(targetHp);
             default:
-                return 0; // ここには到達しないはず
+                return 0;
         }
     }
 
+    /**
+     * 攻撃1
+     * @param targetHp 相手のHP
+     * @return targetHp
+     */
     protected abstract int attack1(int targetHp);
 
+    /**
+     * 攻撃2
+     * @param targetHp 相手のHP
+     * @return targetHp
+     */
     protected abstract int attack2(int targetHp);
 
+    /**
+     * 攻撃3
+     * @param targetHp 相手のHP
+     * @return targetHp
+     */
     protected abstract int attack3(int targetHp);
 
+    /**
+     * 特殊能力
+     * @param targetHp 相手のHP
+     * @return targetHp
+     */
     public abstract int specialAbility(Wizard targetHp);
 
 }
